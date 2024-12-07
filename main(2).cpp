d76d9d2b83c52125396f33e88e0121d56f9aa499
Fabirto <sweatytryhardyes@gmail.com>
2024-12-07 03:50:02 +0300

    Add files via upload

---
+++ main(2).cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <map>
+#include <iostream>
+
+using namespace std;
+
+map<char,int> lettersCount( string str) {
+    map<char,int> letters;
+    
+    for (int i = 0; i < str.size(); i++) {
+        auto c = str[i];
+        letters[c]++;
+}
+return letters;
+}
+
+
+
+int main() {
+    
+    string str = "aaajabbbvvv";
+    map <char, int> ans = lettersCount(str);
+for (auto pos : ans) {
+     cout <<pos.first << " " << pos.second << endl;
+}
+}
